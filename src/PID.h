857e2966270a4919ceed748a71d6cad93eb12021
Gianni Jacklone <gianni.jacklone@gmail.com>
2017-05-23 12:45:04 -0700

    Working PID controller for steering with slow 2 speed throttle

---
+++ src/PID.h
@@ -0,0 +1,54 @@
+#ifndef PID_H
+#define PID_H
+
+class PID {
+public:
+  /*
+  * Errors
+  */
+  double p_error_;
+  double i_error_;
+  double d_error_;
+
+  /*
+  * Coefficients
+  */ 
+  double Kp_;
+  double Ki_;
+  double Kd_;
+
+  /*
+   * Storage Vars
+  */
+  double cte_prev_;
+  double cte_sum_;
+
+  /*
+  * Constructor
+  */
+  PID();
+
+  /*
+  * Destructor.
+  */
+  virtual ~PID();
+
+  /*
+  * Initialize PID.
+  */
+  void Init(double Kp, double Ki, double Kd);
+
+  /*
+  * Update the PID error variables given cross track error.
+  */
+  void UpdateError(double cte);
+
+  /*
+  * Calculate the total PID error.
+  */
+  double TotalError();
+
+  double GetOutput();
+};
+
+#endif /* PID_H */
